537192243d742315e14ac16c7ed1c37b37c0ba73
Shuruder <gabriel_brandini@hotmail.com>
2021-02-05 19:31:11 -0300

    Adicionando Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /*
 Comentário em JS
 */
-
+/*
 var nome = "Shuruder";
 //variaveis podem ser colocadas na mesma linha e são identificadas conforme os dados passados
 var idade = 27, idade2= 21;
@@ -20,3 +20,39 @@ console.log(frase.replace("Japão", "Brasil"));
 //Mostra JAPÃO FAZ PIZZA BOA!
 //Variações como toLowerCase()
 console.log(frase.toUpperCase());
+*/
+
+//Agora aula sobre array e dicionário
+
+//cria o array (lista)
+var lista = ["maça", "pêra", "laranja"];
+
+//mostra a lista como array e todos seus elementos
+console.log(lista)
+
+//incluindo elementos
+lista.push("uva");
+console.log(lista);
+
+//remove o ultimo elemento
+lista.pop();
+console.log(lista);
+
+//tamanho da lista
+console.log(lista.length);
+
+//reverter
+console.log(lista.reverse());
+
+//tranformando em string
+console.log(lista.toString());
+
+// Ao invés de virgula fica o traço na junção ou qualquer outro caractere
+console.log(lista.join(" - "));
+
+// Sobre dicionário agora:
+var fruta = {nome: "maça", cor:"vermelha"}
+console.log(fruta.nome);
+console.log(fruta.cor);
+
+//obs podemos colocar o dicionário em uma lista simplesmente colocando entre [{}]
